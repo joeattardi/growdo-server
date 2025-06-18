da785dd1f8a7f1e92fdc96e387c70efa0d0b8644
Joe Attardi <jattardi@gmail.com>
2025-06-17 21:41:28 -0400

    Use tsx, add basic fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello GrowDo!');
+import Fastify from 'fastify';
+
+const server = Fastify({
+    logger: true
+});
+
+server.get('/', async (request, reply) => {
+    return { hello: 'world' };
+});
+
+async function start() {
+    await server.listen({ port: 3000 });
+    server.log.info(`Server listening on ${server.server.address()}`);
+}
+
+start();
